d9c9fb2354139487e29daa78c4d4fbe0b1630ca0
EcstaticPilot <Nikhil.ramanuja.8@gmail.com>
2021-09-05 13:06:11 -0700

    WIP

---
+++ WIP/7700P_Drive_Code/src/main.cpp
@@ -0,0 +1,137 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       student                                                   */
+/*    Created:      Sat Jul 17 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// LeftBack             motor         11              
+// LeftFront            motor         1               
+// RightBack            motor         20              
+// RightFront           motor         10              
+// Gyro                 inertial      17              
+// RL12                 motor         12              
+// LL18                 motor         18              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+
+#include "vex.h"
+competition Competition;
+using namespace vex;
+float pi = 3.14;
+float Diamter = 4.0;
+
+void drive(int lspeed, int rspeed,  int dtime){
+  LeftBack.spin(forward, lspeed, pct);
+  LeftFront.spin(forward, lspeed, pct);
+  RightFront.spin(forward, rspeed, pct);
+  RightFront.spin(forward, rspeed, pct);
+  wait(dtime, msec); 
+}
+
+void pre_auton()
+{
+  wait(500, msec);
+}
+
+void auton()
+{
+ 
+}
+
+void inchDrive(float target, int speed){
+  LeftFront.setPosition(0, rev);
+  float inches = 0.0;
+  float turns=0.0;
+  while (fabs(inches) < fabs(target)) {
+    drive(speed, speed ,10);
+    turns = LeftFront.position(rev);
+    inches = turns * Diamter * pi;
+    Brain.Screen.printAt(1,100, "turns = %.2f inches=%.2f", turns, inches);
+  }
+  drive(0,0,0);
+}
+//need to add a gyro
+//PID TURN D = DERIVITIVE, SLOPE OF A FUNCTION
+void gyroturn(float target){ //turning gyro 
+  float kp=2.0;
+  float kd = 16.0;
+  Gyro.setRotation(0, degrees); //setting gyro to 0
+  float heading = 0.0;
+  float speed = 100;
+  float error = target;
+  float olderror=error;
+  while(fabs(error)>2.0){ //fabs = absolute value
+    heading= Gyro.rotation(degrees);
+    olderror=error;
+    error = target-heading; //the error gets smaller when u reach ur target
+    drive(speed, -speed, 10);
+    speed = kp*error+kd*(error-olderror); //when error is big go fast when error is small go slow, find new error
+    Brain.Screen.printAt(1, 40, "heading = %0.2f    degrees", heading); // formula for 2 decimal places
+    Brain.Screen.printAt(1, 60, "speed = %0.2f    degrees", speed);
+  }
+  drive(0,0,0);
+  heading= Gyro.rotation(degrees);
+  Brain.Screen.printAt(1, 40, "heading = %0.2f    degrees", heading);
+}
+
+float scale = 12000/100;
+
+void driver(){
+  
+  Brain.Screen.printAt(1,  40,  "Hello  Friends Driver is Running");
+  // User control code here, inside the loop
+  while (true){
+
+  
+    LL18.setStopping(hold); 
+    RL12.setStopping(hold);
+    if(Controller1.ButtonLeft.pressing()){
+      LL18.spin(forward, 75, pct);
+      RL12.spin(forward, 75, pct);
+  }
+    else if(Controller1.ButtonDown.pressing()){
+      LL18.spin(reverse, 75, pct);
+      RL12.spin(reverse, 75, pct);
+
+  }
+    else{
+      LL18.stop();
+      RL12.stop();
+  }
+
+  
+    LeftBack.spin(forward, Controller1.Axis3.position()*scale,voltageUnits::mV); 
+    LeftFront.spin(forward, Controller1.Axis3.position()*scale,voltageUnits::mV); 
+    RightBack.spin(forward, Controller1.Axis2.position()*scale,voltageUnits::mV); 
+    RightFront.spin(forward, Controller1.Axis2.position()*scale,voltageUnits::mV);
+    
+    wait(10, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+ 
+
+
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(auton);
+  Competition.drivercontrol(driver);
+  
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+
+  }
+
+}
---
+++ WIP/7700P_Drive_Code/src/robot-config.cpp
@@ -0,0 +1,31 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor LeftBack = motor(PORT11, ratio18_1, false);
+motor LeftFront = motor(PORT1, ratio18_1, false);
+motor RightBack = motor(PORT20, ratio18_1, true);
+motor RightFront = motor(PORT10, ratio18_1, true);
+inertial Gyro = inertial(PORT17);
+motor RL12 = motor(PORT12, ratio18_1, true);
+motor LL18 = motor(PORT18, ratio18_1, false);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
