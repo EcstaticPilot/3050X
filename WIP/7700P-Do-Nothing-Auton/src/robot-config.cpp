fefb1705bc15d0dc878bafe72c934d49a5f10314
EcstaticPilot <Nikhil.ramanuja.8@gmail.com>
2021-09-17 18:35:35 -0700

    Abby
    Do Nothing Auton Skills

---
+++ WIP/7700P-Do-Nothing-Auton/src/robot-config.cpp
@@ -0,0 +1,32 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor RBDrive = motor(PORT20, ratio18_1, true);
+motor LBDrive = motor(PORT11, ratio18_1, false);
+motor LLift = motor(PORT12, ratio18_1, true);
+controller Controller1 = controller(primary);
+motor LFDrive = motor(PORT1, ratio18_1, false);
+inertial Gyro = inertial(PORT13);
+motor RLift = motor(PORT19, ratio18_1, false);
+digital_out Claw = digital_out(Brain.ThreeWirePort.A);
+motor RFDrive = motor(PORT10, ratio18_1, true);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
---
+++ WIP/7700P-Leftside-auton-skills-code /src/main.cpp
@@ -0,0 +1,230 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\chiep                                            */
+/*    Created:      Tue Oct 06 2020                                           */
+/*    Description:  9-1-21 added pneumatics                                              */
+/*                   abby added left auton 9/17/21                                                         */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// RBDrive              motor         20              
+// LBDrive              motor         11              
+// LLift                motor         12              
+// Controller1          controller                    
+// LFDrive              motor         1               
+// Gyro                 inertial      13              
+// RLift                motor         19              
+// Claw                 digital_out   A               
+// RFDrive              motor         10              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+competition Competition;
+//Global Variable
+float dia = 4.0;
+
+//tis is GUI : Graphic User Interface
+
+
+void Drive(int lspeed, int rspeed){
+  LBDrive.spin(forward, lspeed, pct);
+  RBDrive.spin(forward, rspeed, pct);
+  LFDrive.spin(forward, lspeed, pct);
+  RFDrive.spin(forward, rspeed, pct);
+}
+
+void claw(bool claw){
+  Claw.set(claw);
+}
+
+void lift(int liftspeed){
+  LLift.spin(forward, liftspeed, pct);
+  RLift.spin(forward, liftspeed, pct);
+}
+void draw(){
+
+Brain.Screen.setFillColor(red);
+Brain.Screen.setPenColor(white);
+Brain.Screen.drawRectangle(10, 10, 100, 50);
+Brain.Screen.printAt(10, 10, "hi");
+Brain.Screen.printAt( 10 , 20,"RMotor Temp%f ",LBDrive.temperature(pct));
+Brain.Screen.printAt( 10, 35,"LMotor Temp%f ",RBDrive.temperature(pct));
+//Brain.Screen.printAt(10, 40,"Motor");
+
+
+Brain.Screen.setFillColor(blue);
+Brain.Screen.setPenColor(white);
+Brain.Screen.drawRectangle(400, 10, 80, 50);
+Brain.Screen.printAt(410, 15, "C++" );
+
+
+
+}
+
+void motorCurrent() { 
+ /* Brain.Screen.printAt( 10, 80,"LMotor current%f  ",LeftBackMotor.current());
+  Brain.Screen.printAt( 10, 96,"RMotor current%f  ",RightBackMotor.current());
+  Brain.Screen.printAt( 10, 110,"LMotor current%f ",Lfront.current());
+  Brain.Screen.printAt( 10, 122,"RMotor current%f ",Rfront10.current());
+  Brain.Screen.printAt( 10, 132,"Motor current%f  ",Arm.current());
+  */
+
+
+
+}
+
+void autonDriver(int wt, int lspeed, int rspeed, int liftspeed, bool claw) 
+{
+
+  LBDrive.spin(forward, lspeed, pct);
+  RBDrive.spin(forward, rspeed, pct);
+  LFDrive.spin(forward, lspeed, pct);
+  RFDrive.spin(forward, rspeed, pct); 
+  LLift.spin(forward, liftspeed, pct);
+  RLift.spin(forward, liftspeed, pct);
+  Claw.set(claw);
+
+wait(wt, msec);
+}
+
+ void inchDrive (float target, int speed, bool claw) {
+Claw.set(claw);
+  float c = 0; //our distance
+LBDrive.setRotation(0, degrees);
+  while (fabs (c) <= target) {
+
+    LBDrive.spin(forward,speed, pct);
+    RBDrive.spin(forward, speed, pct);
+    LFDrive.spin(forward, speed, pct);
+    RFDrive.spin(forward, speed, pct);
+    
+   c = LBDrive.rotation(rev)*3.14*dia; 
+  }
+   autonDriver(0, 0, 0, 0, false);
+    
+}
+
+void gyroTurn(float target, int Lspeed, int Rspeed, bool claw)
+{
+  while(Gyro.isCalibrating())
+{
+  // Wait for Gyro Calibration , Sleep but Allow other tasks to run
+    this_thread::sleep_for(20);
+}
+  float heading4 = 0;
+  Gyro.setRotation(0, degrees); 
+ 
+float speed=0.0 ;
+float kp = 2.0;
+  while (fabs(heading4)<= target) 
+{
+    speed=kp*(target-heading4);
+    autonDriver(10 , Lspeed, Rspeed, 0, claw);
+    wait(10,msec);
+    heading4=Gyro.rotation(degrees); 
+  }
+  autonDriver(0, 0, 0, 0, true);
+}
+////////////----------------------EOF-----------------------//////////////////////
+
+void pre_auton(void) {
+
+}
+
+ void autonomous(void) {
+    while(Gyro.isCalibrating())
+{
+  
+    wait(20,msec);
+}
+// autonDriver(wait time, left drive speed, right drive speed, lift speed, claw close is true) 
+Brain.Screen.printAt( 20, 20,"Motor Temp%f ",LBDrive.temperature(pct));
+Brain.Screen.printAt( 20, 40,"Motor Temp%f ",RBDrive.temperature(pct));
+Brain.Screen.printAt( 20, 60,"Motor Temp%f ",LFDrive.temperature(pct));
+Brain.Screen.printAt( 20, 80,"Motor Temp%f ",RFDrive.temperature(pct));
+ Brain.Screen.printAt( 20, 100,"Heading%f ",Gyro.rotation(deg));
+ //claw true is closed, false is open
+ 
+ /*
+ 
+ 
+ inchDrive(2, 75, true);
+ inchDrive(0, 75, false);*/
+}
+
+  
+
+ void driverControl () {
+
+   while (true){
+     if (Controller1.ButtonR2.pressing())
+     {
+       Claw.set(true);
+     }
+     else if(Controller1.ButtonR1.pressing())
+     {
+       Claw.set(false);
+     }
+
+     
+   Brain.Screen.printAt(160, 60, "hi");
+Brain.Screen.printAt( 20, 20,"LB Motor Temp%f ",LBDrive.temperature(pct));
+Brain.Screen.printAt( 20, 40,"RB Motor Temp%f ",RBDrive.temperature(pct));
+Brain.Screen.printAt( 20, 60,"LF Motor Temp%f ",LFDrive.temperature(pct));
+Brain.Screen.printAt( 20, 80,"RF Motor Temp%f ",RFDrive.temperature(pct));
+
+  LBDrive.spin(forward, Controller1.Axis3.position(pct), pct);
+  LFDrive.spin(forward, Controller1.Axis3.position(pct), pct);
+  RFDrive.spin(forward, Controller1.Axis2.position(pct), pct);
+  RBDrive.spin(forward, Controller1.Axis2.position(pct), pct);
+
+ if (Controller1.ButtonL1.pressing()) {
+    LLift.spin(forward, 100, pct);
+    RLift.spin(forward, 100, pct);
+
+    
+   }
+   else if 
+     (Controller1.ButtonL2.pressing()) {
+       LLift.spin(reverse, 100, pct);
+       RLift.spin(reverse, 100, pct);
+     }
+  
+ 
+ else {
+       LLift.stop(brake);
+       RLift.stop(brake);
+
+
+ }}
+  }
+
+
+
+
+
+
+int main() {
+  vexcodeInit();
+  
+Competition.autonomous(autonomous);
+  Competition.drivercontrol(driverControl);
+
+
+    pre_auton();
+   
+   
+    //autonomous ();
+    //driverControl(); 
+while (true){
+wait (100, msec); 
+}
+    // Run the pre-autonomous function.
+  }
+  // Initializing Robot Configuration. DO NOT REMOVE!
+
