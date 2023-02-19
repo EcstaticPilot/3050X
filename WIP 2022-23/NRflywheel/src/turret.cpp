c24641b13927619384bcc59e231e9155cb800ecd
Areophix <Morgan.deng@icloud.com>
2023-02-18 21:59:57 -0800

    help
    i tried to fix the merging but i don understand >:(

--- WIP 2022-23/NRflywheel/src/turret.cpp
+++ WIP 2022-23/NRflywheel/src/turret.cpp
@@ -2,23 +2,17 @@
 #include "vision.h"
 #include <algorithm>
 #include <iostream>
-<<<<<<< HEAD
-
-bool loading=false;
-=======
 #include <math.h>
 #include <vector>
-bool loading = false;
->>>>>>> nik_things
+
+bool loading=false;
 double GoalAngle;
 extern double X, Y;
 float offset = 0;
 bool VisionReady;
 int mode = 0;
 extern bool isRed;
-<<<<<<< HEAD
 
-=======
 int findClosetObject(int a, int b){
   if(abs(140-Vision16.objects[a].centerX)
   <abs(140-Vision16.objects[b].centerX))
@@ -26,10 +20,10 @@ int findClosetObject(int a, int b){
     return a;
   }
   else {
-  return b;
+    return b;
   }
 }
->>>>>>> nik_things
+
 int turretStable() {
   Controller1.rumble("..");
   loading = true;
@@ -46,91 +40,84 @@ int turretStable() {
   double speed;
 
   while (true) {
-<<<<<<< HEAD
     GoalAngle = atan2(X - 100, 120 - Y) * (180 / M_PI);
-    //GoalAngle=atan2(Controller2.Axis2.value(),Controller2.Axis1.value())*180/M_PI; if(Controller2.Axis2.position()<5&&Controller2.Axis1.position()<5)loading=true;else loading=false;
-    
-    if (!loading) {
-          std::cout<<GoalAngle- turretG.orientation(yaw, degrees)<<std::endl;
-      if (fabs(GoalAngle- turretG.orientation(yaw, degrees)) <
-          50) {
-            if(isRed){ Vision16.takeSnapshot(RGOAL);
-            }else {
-            Vision16.takeSnapshot(BGOAL);
-            }                        // if goal is within limits
-        // use vision sensor
-        if (Vision16.largestObject.exists) {
 
+    if (!loading) {
+      std::cout<<GoalAngle- turretG.orientation(yaw, degrees)<<std::endl;
+      if (fabs(GoalAngle- turretG.orientation(yaw, degrees)) < 50) {
+        if(isRed) { 
+          Vision16.takeSnapshot(RGOAL);
+        }
+        else {
+          Vision16.takeSnapshot(BGOAL);
+        }
+        if (Vision16.largestObject.exists) {
           error = Vision16.largestObject.centerX - 140;
 
           Brain.Screen.printAt(1, 100, "error  = %.1f      ", error);
-          Brain.Screen.printAt(1, 160, "vision  = %.1f      ",
-                               Vision16.largestObject.centerX);
-          mode=1;
+          Brain.Screen.printAt(1, 160, "vision  = %.1f      ", Vision16.largestObject.centerX);
+
+          mode = 1;
           kp = 0.2;
-          //kd = .09;
-          if(fabs(error)<20)VisionReady=true;
+
+          if(fabs(error)<20) VisionReady=true;
           else VisionReady=false;
-        } else {
+        } 
+        else {
           VisionReady=false;
           error = -(GoalAngle + offset) - turretG.orientation(yaw, degrees);
           kp = 1;
           kd = 0.3;
           mode=2;
-=======
-    GoalAngle = atan2(X - 115, 115 - Y) * (180 / M_PI);
+          GoalAngle = atan2(X - 115, 115 - Y) * (180 / M_PI);
+
+////////////////////////////////////////////////////////////////
 
     if (!loading) {
 
       if (fabs(GoalAngle + offset - turretG.orientation(yaw, degrees)) < 300) {
-        if (isRed)
+        if (isRed) {
           Vision16.takeSnapshot(RGOAL);
-        else
+        }
+        else {
           Vision16.takeSnapshot(BGOAL);
-        // if goal is within limits
-        // use vision sensor
-        // find closest
+        }
         int closestObject=0;
-        //int i=0;
-        for(int i=0; i<5; ++i){
-        //while(i<7){
+
+        for(int i=0; i<5; ++i) {
           closestObject=findClosetObject(closestObject, i);
-         // i++;
->>>>>>> nik_things
         }
         //try this? https://www.sanfoundry.com/cpp-program-minimum-element-array-using-linear-search-2/
-
      
         error = Vision16.objects[closestObject].centerX - 140;
 
         Brain.Screen.printAt(1, 100, "error  = %.1f      ", error);
-        Brain.Screen.printAt(1, 160, "vision  = %.1f      ",
-                             Vision16.largestObject.centerX);
+        Brain.Screen.printAt(1, 160, "vision  = %.1f      ", Vision16.largestObject.centerX);
+
         mode = 1;
         kp = 0.5;
         kd = .3;
+
         if (fabs(error) < 20)
           VisionReady = true;
         else
           VisionReady = false;
-      } else {
+      } 
+      else {
         VisionReady = false;
         error = -(GoalAngle + offset) - turretG.orientation(yaw, degrees);
         kp = 1;
         kd = 0.3;
         mode = 2;
       }
-      //    VisionReady=false;//vision is not ready
-    } else { // if not loading go to zero
-
-<<<<<<< HEAD
-        error = TurretE.position(turns)*360;
-      
-          kp = 0.5;
-          kd = 0.3;
-          mode=4;
-        VisionReady = false; // vision is not ready
-      }
+    } 
+    else { // if not loading go to zero
+      error = TurretE.position(turns)*360;
+      kp = 0.5;
+      kd = 0.3;
+      mode=4;
+      VisionReady = false; 
+    }
 
       speed = (error * kp) + (ki * sum) + (kd * (error - prevError));
       if ((speed > 0 && TurretE.position(turns)*360 < -120))
@@ -142,28 +129,29 @@ int turretStable() {
       prevError = error;
       sum += error;
       this_thread::sleep_for(10);
-=======
+
       error = TurretE.position(turns) * 360;
       std::cout << error << std::endl;
       kp = 0.5;
       kd = 0.3;
       mode = 4;
       VisionReady = false; // vision is not ready
->>>>>>> nik_things
     }
 
-    speed = (error * kp) + (ki * sum) + (kd * (error - prevError));
-    if ((speed > 0 && TurretE.position(turns) * 360 < -120))
-      speed = 0;
-    if ((speed < 0 && TurretE.position(turns) * 360 > 180))
-      speed = 0;
-    turret.spin(fwd, speed, pct);
+    // speed = (error * kp) + (ki * sum) + (kd * (error - prevError));
+    // if ((speed > 0 && TurretE.position(turns) * 360 < -120))
+    //   speed = 0;
+    // if ((speed < 0 && TurretE.position(turns) * 360 > 180))
+    //   speed = 0;
+    // turret.spin(fwd, speed, pct);
 
-    prevError = error;
-    sum += error;
-    this_thread::sleep_for(10);
+    // prevError = error;
+    // sum += error;
+    // this_thread::sleep_for(10);
   }
   return 1;
+}
+}
 }
 
 void turretSpinTo(double targetAngle, bool global) {
