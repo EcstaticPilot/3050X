a299ba757231bd0a0407d8c142c22ccaaf898dc9
Nikhil Ramanuja <Nikhil.ramanuja.8@gmail.com>
2023-06-10 11:15:14 -0700

    it does work

--- WIP 2022-23/Stanley/src/drive.cpp
+++ WIP 2022-23/Stanley/src/drive.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <vector>
 #include <math.h>
-float TrackWidth = 17/2;
+float TrackWidth = 17 / 2;
 float C = M_PI * 3.25;
 extern double X, Y;
 void drive(int lSpeed, int rSpeed, double wt)
@@ -295,9 +295,9 @@ double DegToRad(double deg)
 
 bool zeroCrossing(double a, double b)
 {
-  int A=fabs(a)/a;
-  int B=fabs(b)/b;
-  if(A!=B)
+  int A = fabs(a) / a;
+  int B = fabs(b) / b;
+  if (A != B)
   {
     return true;
   }
@@ -307,9 +307,6 @@ bool zeroCrossing(double a, double b)
   }
 }
 // a class containing the x and y coordinates of a point
-
-
-
 
 /**
  * @brief finds the slope between two points
@@ -323,7 +320,7 @@ float slope(double points[][2], int point1, int point2)
   double y1 = points[point1][1];
   double x2 = points[point2][0];
   double y2 = points[point2][1];
-  double slope = RadToDeg(atan2(x2-x1,y2-y1));
+  double slope = RadToDeg(atan2(x2 - x1, y2 - y1));
   return slope;
 }
 
@@ -335,24 +332,28 @@ float slope(double points[][2], int point1, int point2)
 double robotDistance(double x, double y)
 {
 
-  return (sqrt((X-x)*(X-x) + (Y-y)*(Y-y)));
+  return (sqrt((X - x) * (X - x) + (Y - y) * (Y - y)));
 }
 
-int signOfDistance(double points[][2], int point1, int point2){
+int signOfDistance(double points[][2], int point1, int point2)
+{
   double x1 = points[point1][0];
   double y1 = points[point1][1];
   double x2 = points[point2][0];
   double y2 = points[point2][1];
   double x3 = X;
   double y3 = Y;
-  double d = (x2-x1)*(y1-y3) - (x1-x3)*(y2-y1);
-  if (d > 0){
+  double d = (x2 - x1) * (y1 - y3) - (x1 - x3) * (y2 - y1);
+  if (d > 0)
+  {
     return -1;
   }
-  else if (d < 0){
+  else if (d < 0)
+  {
     return 1;
   }
-  else{
+  else
+  {
     return 0;
   }
 }
@@ -362,14 +363,13 @@ int signOfDistance(double points[][2], int point1, int point2){
  * @param y y coordinate of the point
  * @param speed speed to drive at
  */
-void curveDrive2(double x, double y, double speed){
-  double L = robotDistance(x,y);
-  double errorX= x-X;
-  double errorY= y-Y;
-  double localX = errorX*cos(gyro1.rotation()) - errorY*sin(gyro1.rotation());
-  double curvature = 2*localX/(L*L);
-
-
+void curveDrive2(double x, double y, double speed)
+{
+  double L = robotDistance(x, y);
+  double errorX = x - X;
+  double errorY = y - Y;
+  double localX = errorX * cos(gyro1.rotation()) - errorY * sin(gyro1.rotation());
+  double curvature = 2 * localX / (L * L);
 }
 
 /**
@@ -380,27 +380,28 @@ int closestPoint(double points[][2])
 {
   double min = robotDistance(points[0][0], points[0][1]);
   int index = 0;
-  
-  
- 
-  for (int i =0; i < sizeof points; i++)
+
+  for (int i = 0; i < sizeof points; i++)
   {
     double dist = robotDistance(points[i][0], points[i][1]);
-    if (dist < min) {min = dist; index = i;}
-    
+    if (dist < min)
+    {
+      min = dist;
+      index = i;
+    }
   }
   return index;
 }
 
 int nextPoint(int initial, double points[][2])
 {
-  if (robotDistance(points[initial][0], points[initial][1]) < robotDistance(points[initial+1][0], points[initial+1][1]))
+  if (robotDistance(points[initial][0], points[initial][1]) < robotDistance(points[initial + 1][0], points[initial + 1][1]))
   {
     return initial;
   }
   else
   {
-    return initial+1;
+    return initial + 1;
   }
 }
 /**
@@ -418,10 +419,10 @@ float perpendicularDist(double points[][2], int point1, int point2)
   double x3 = X;
   double y3 = Y;
   // calculate distance
-  double A=y2-y1;
-  double B=x1-x2;
-  double C=x2*y1-x1*y2;
-  double d = fabs((A*x3+B*y3+C))/sqrt(A*A+B*B);
+  double A = y2 - y1;
+  double B = x1 - x2;
+  double C = x2 * y1 - x1 * y2;
+  double d = fabs((A * x3 + B * y3 + C)) / sqrt(A * A + B * B);
   // check for intersection
   double a = robotDistance(x1, y1);
   double b = distance2points(x1, y1, x2, y2);
@@ -441,55 +442,55 @@ float perpendicularDist(double points[][2], int point1, int point2)
  * @brief stanley controller for following a path of points
  * @param points an array containing points to follow
  */
-void stanley(double points[][2],int length)
+void stanley(double points[][2], int length)
 {
-  double kp=0.75;
-  double ki=0;
-  double kd=0;
+  double kp = 0.75;
+  double ki = 0;
+  double kd = 0;
   double ld;
-  double v = 50;
+  double v;
   double kv = 1;
   double pathDistance;
   double pathHeading;
   double ldAngle;
-  double prevError=0; 
-  double totalError=0;
+  double prevError = 0;
+  double totalError = 0;
   int sign;
- Brain.Screen.print("stanley");
+  Brain.Screen.print("stanley");
   int i = 0;
-  int pointClosest=0;
+  int pointClosest = 0;
   while (true)
   {
     i++;
-    
+
     Brain.Screen.print("%.1f", i);
     // find the closest point
-    //pointClosest =closestPoint(points);
+    // pointClosest =closestPoint(points); //alternate way to find closest point
     pointClosest = nextPoint(pointClosest, points);
     Brain.Screen.print("0.5");
     // if the closest point is the last point, stop
-    if (pointClosest== length-1)
+    if (pointClosest == length - 1)
     {
-     break;
+      break;
     }
-     Brain.Screen.print("1");
+
     // find the distance to the line segment before and after the closest point
-    //distance of the path segment before the closest point
-    double segment1dist = (!(pointClosest == 0) ? (perpendicularDist(points, (pointClosest - 1), pointClosest) ):( 900000000000));
-    //distance of the path segment after the closest point
-    double segment2dist = perpendicularDist(points, pointClosest, pointClosest + 1);
+
+    double segment1dist = (!(pointClosest == 0) ? (perpendicularDist(points, (pointClosest - 1), pointClosest)) : (900000000000)); // distance of the path segment before the closest point
+
+    double segment2dist = perpendicularDist(points, pointClosest, pointClosest + 1); // distance of the path segment after the closest point
     // if neither one has an intersection, find the distance to the line segment after the next point
-     Brain.Screen.print("2");
-    if (segment1dist>300000 && segment2dist>300000 )
+
+    if (segment1dist > 300000 && segment2dist > 300000)
     {
-      segment2dist-=300000;
+      segment2dist -= 300000;
     }
+
     // find the distance to the closest line segment and the heading of that line segment
-    Brain.Screen.print("3");
     if (segment2dist < segment1dist)
     {
       pathDistance = segment2dist;
-      pathHeading = slope(points, pointClosest, pointClosest + 1);
+      pathHeading = slope(points, pointClosest, pointClosest + 1); //
       sign = signOfDistance(points, pointClosest, pointClosest + 1);
     }
     else
@@ -498,37 +499,45 @@ void stanley(double points[][2],int length)
       pathHeading = slope(points, pointClosest - 1, pointClosest);
       sign = signOfDistance(points, pointClosest - 1, pointClosest);
     }
-     Brain.Screen.print("4");
-    ld =7.5;// v / kv;
-    // calculate the angle to the line segment and the error in the heading
-    ldAngle = RadToDeg(atan2(pathDistance, ld));
-   
-    // drive the robot
-    double error = ( ldAngle*sign + pathHeading-gyro1.yaw(deg));
-    //start pid
-    double output=(error*kp)+(totalError)*ki+(prevError-error)*kd;
-
-    prevError=error;
-    drive(25+output,25-output,10);
-    //reset integral if error crosses zero
-    if(zeroCrossing(error,prevError)){
-      totalError=0;
-    }
-    else{
-      totalError+=error;
-    }
-    //curveDrive(25,( ldAngle*-sign + pathError-gyro1.yaw(deg)/5));
-    // update speed
+    // calculate lookahead distance
     v = LF.velocity(pct) + RF.velocity(pct) / 2;
-    // print the values
-    //pathDistance<<","<<ldAngle*-sign<<","<<perpendicularDist(points,0,1)<<","
-  //  std::cout<<"x="<<X<<", y= "<<Y<<", pointclosest= "<<pointClosest<<" error= "<<error<<std::endl
-  //  <<"seg1dist= "<<segment1dist<<", seg2dist= "<<segment2dist<<", pathdist= "<<pathDistance<<std::endl<<std::endl;
-  std::cout<<X<<","<<Y<<","<<pointClosest<<std::endl;
-    wait(10, msec);
-  } 
-  drive(0,0,100);
-  drive_brake;                                                                                                                                                              
-  std::cout<<"done"<<std::endl;
-}
+    ld = 7.5; // v / kv;
 
+    // calculate the angle to the global angle to the lookahead point
+    ldAngle = RadToDeg(atan2(pathDistance, ld));
+
+    // calculate the error
+    double error = (ldAngle * sign + pathHeading - gyro1.yaw(deg));
+
+    // calculate the PID output
+    double output = (error * kp) + (totalError)*ki + (prevError - error) * kd;
+
+    // prevoius error
+    prevError = error;
+
+    // drive
+    drive(25 + output, 25 - output, 10);
+
+    // reset integral if error crosses zero
+    if (zeroCrossing(error, prevError))
+    {
+      totalError = 0;
+    }
+    else
+    {
+      totalError += error;
+    }
+    // curveDrive(25,( ldAngle*-sign + pathError-gyro1.yaw(deg)/5));
+    //  update speed
+
+    // print the values
+    // pathDistance<<","<<ldAngle*-sign<<","<<perpendicularDist(points,0,1)<<","
+    //  std::cout<<"x="<<X<<", y= "<<Y<<", pointclosest= "<<pointClosest<<" error= "<<error<<std::endl
+    //  <<"seg1dist= "<<segment1dist<<", seg2dist= "<<segment2dist<<", pathdist= "<<pathDistance<<std::endl<<std::endl;
+    std::cout << X << "," << Y << "," << pointClosest << std::endl;
+    wait(10, msec);
+  }
+  drive(0, 0, 100);
+  drive_brake;
+  std::cout << "done" << std::endl;
+}
