872b777b4c538ea76a9db5c439f20f65725b07f6
Nikhil Ramanuja <Nikhil.ramanuja.8@gmail.com>
2023-09-23 10:23:41 -0700

    wrong version now

--- OverUnder23-24/XiphosV1/src/main.cpp
+++ OverUnder23-24/XiphosV1/src/main.cpp
@@ -4,14 +4,14 @@
 /*    Author:       Nicool Ramanuja                                           */
 /*    Created:      9/3/2023, 5:15:15 PM                                     */
 /*    Description:  program for palos verdes peninsula high school team 3050X */
-/*                  robot version 0                                           */
+/*                  robot version WRONG VERSION                               */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
 #include "vex.h"
 #include "sylib/sylib.hpp"
 using namespace vex;
-
+//!WRONG CODE VERSION
 extern sylib::Addrled LED;
 // A global instance of competition
 competition Competition;
